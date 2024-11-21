185bcb8b43b59be3da57c122b0ae70354130be34
meisibley <mkong99@yahoo.com>
2024-11-21 15:01:45 -0600

    transaction history

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
+import TransactionHistory from '@/components/TransactionHistory/page';
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="bg-white">
+      <section>
+        <div className="w-full min-h-screen overflow-x-hidden m-0 p-0">
+          <TransactionHistory />
+        </div>
+      </section>
+    </div>
   );
 }
