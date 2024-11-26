fc784bbfcf75c635037df3c97538198f0bf07325
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-11-25 21:45:35 -0600

    SP 9:45 PM - Cleaned Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
-import ChildProfileCard
- from "@/components/ChildCard/ChildProfileCard";
 export default function Home() {
   return (
     <>
-    <ChildProfileCard
-      childName="Samantha"
-      profileImageUrl="/images/samantha.png"
-      checkingBalance={100}
-      savingsBalance={50}
-      savingsGoalPercentage={40}
-      loanRepaymentPercentage={60}
-    />
     </>
   );
 }
