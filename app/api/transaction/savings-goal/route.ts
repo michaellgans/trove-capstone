8e60c77219e5bbc34ff31a0f3d70627115aef18f
Jtownokie <christopher.stephens@atlasschool.com>
2024-12-04 15:28:01 -0600

    CRS - 3:27PM - Completed savings goal update route

--- app/api/transaction/savings-goal/route.ts
+++ app/api/transaction/savings-goal/route.ts
@@ -2,6 +2,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import verifyToken from "@/lib/verifyToken";
 import { prisma } from "@/prisma";
+import { dollarsToCents } from "@/lib/utils";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -10,6 +11,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!childUser) {
       return res.status(401).json({ error: "Unauthorized" });
     }
+
+    const amount = req.body.amount;
+    const convertedAmount = dollarsToCents(amount);
+
+    await prisma.child_account.update({
+      where: {
+        child_id: childUser.id,
+      },
+      data: {
+        savings_goal: convertedAmount,
+      },
+    });
+
+    res.status(200).json({ message: "Set Savings Goal" });
   } catch (error) {
     return res.status(500).json({ error: "Failed to update savings goal" });
   }
