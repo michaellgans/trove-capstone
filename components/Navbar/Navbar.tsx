ad2b3d9f0bde79ca1458c259e1b1a02416050573
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-12-06 14:19:02 -0600

    SP - 2:17 - The navbar got some more styling on hover on collapse, the transparent background, hamburger button, some mobile screen logic. Currently it is totally done for desktop in terms of styling, but needs items for mobile and tablet

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -8,6 +8,8 @@ import DropdownMenu from './DropdownMenu';
 import { useRouter } from 'next/navigation';
 
 
+
+
 interface FamilyMember {
   id: number;
   name: string;
@@ -16,12 +18,13 @@ interface FamilyMember {
 
 
 const Navbar: FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
   const [selectedFamilyMember, setSelectedFamilyMember] = useState<string | number>('');
   const [selectedLesson, setSelectedLesson] = useState<string | number>('');
   const [isFamilyDropdownOpen, setIsFamilyDropdownOpen] = useState<boolean>(false);
   const [isLearningDropdownOpen, setIsLearningDropdownOpen] = useState<boolean>(false);
   const [isMyBalanceDropdownOpen, setIsMyBalanceDropdownOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const familyDropdownRef = useRef<HTMLDivElement>(null);
   const learningDropdownRef = useRef<HTMLDivElement>(null);
@@ -29,13 +32,39 @@ const Navbar: FC = () => {
 
   const router = useRouter();
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) setIsOpen(false);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (isOpen) document.body.classList.add('overflow-hidden');
+    else document.body.classList.remove('overflow-hidden');
+  }, [isOpen]);
+
   // Dummy data for family members and lessons
 
   const familyMemberActions = [
     {
       id: 'send-money',
       label: 'Send Money',
-      icon: '/images/add.svg',
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor" // Use currentColor to inherit color from the parent
+          className="h-6 w-6 transition-colors"
+          >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+        </svg>
+
+      ),
       action: (member: FamilyMember) => {
         console.log(`Sending money to ${member.name}`);
         router.push(`/send?to=${member.id}`);
@@ -44,7 +73,23 @@ const Navbar: FC = () => {
     {
       id: 'view-history',
       label: 'View History',
-      icon: '/images/history.svg',
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="h-6 w-6 transition-colors"
+        >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v7.5m2.25-6.466a9.016 9.016 0 0 0-3.461-.203c-.536.072-.974.478-1.021 1.017a4.559 4.559 0 0 0-.018.402c0 .464.336.844.775.994l2.95 1.012c.44.15.775.53.775.994 0 .136-.006.27-.018.402-.047.539-.485.945-1.021 1.017a9.077 9.077 0 0 1-3.461-.203M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+          />
+        </svg>
+
+      ),
       action: (member: FamilyMember) => {
         console.log(`Viewing history for ${member.name}`);
         router.push(`/home/#history?member=${member.id}`);
@@ -53,7 +98,28 @@ const Navbar: FC = () => {
     {
       id: 'settings',
       label: 'Settings',
-      icon: '/images/settings.svg',
+      icon: (
+        <svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth="1.5"
+  stroke="currentColor"
+  className="h-6 w-6 transition-colors"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+  />
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+  />
+</svg>
+
+      ),
       action: (member: FamilyMember) => {
         console.log(`Viewing settings for ${member.name}`);
         router.push(`/settings?member=${member.id}`);
@@ -86,18 +152,62 @@ const Navbar: FC = () => {
     {
       id: 'balance1',
       label: 'Trove Checking Account 5555',
-      icon: '/images/dollars.svg',
+      icon: (
+        <svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth="1.5"
+  stroke="currentColor"
+  className="h-6 w-6 transition-colors"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+  />
+</svg>
+
+      ),
       children: [
         {
           id: 'balance',
           label: '$1,000',
-          icon: '/images/dollars.svg',
+          icon: (
+            <svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth="1.5"
+  stroke="currentColor"
+  className="h-6 w-6 transition-colors"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+  />
+</svg>
+
+          ),
           action: () => alert('Balance clicked!'),
         },
         {
           id: 'add-balance',
           label: 'Add to My Balance',
-          icon: '/images/add.svg',
+          icon: (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor" // Use currentColor to inherit color from the parent
+              className="h-6 w-6 transition-colors"
+              >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+            </svg>
+    
+          ),
           action: () => router.push('/add-balance'),
         },
       ],
@@ -105,18 +215,66 @@ const Navbar: FC = () => {
     {
       id: 'withholdings',
       label: 'Withholdings Balance',
-      icon: '/images/bank.svg',
+      icon: (
+        <svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth="1.5"
+  stroke="currentColor"
+  className="h-6 w-6 transition-colors"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z"
+  />
+</svg>
+
+      ),
       children: [
         {
           id: 'transfer-withholdings',
           label: '$50.05',
-          icon: '/images/dollars.svg',
+          icon: (
+            <svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth="1.5"
+  stroke="currentColor"
+  className="h-6 w-6 transition-colors"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+  />
+</svg>
+
+          ),
           action: () => alert('Transfer Withholdings clicked!'),
         },
         {
           id: 'transfer-withholdings',
           label: 'Transfer Withholdings',
-          icon: '/images/logout.svg',
+          icon: (
+            <svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth="1.5"
+  stroke="currentColor"
+  className="h-6 w-6 transition-colors"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+  />
+</svg>
+
+          ),
           action: () => alert('Transfer Withholdings clicked!'),
         },
       ],
@@ -127,6 +285,7 @@ const Navbar: FC = () => {
   const handleLogout = (): void => {
     setIsLoggedIn(false);
   };
+  const handleClose = () => setIsOpen(false);
 
   const handleClickOutside = (event: MouseEvent) => {
     if (familyDropdownRef.current && !familyDropdownRef.current.contains(event.target as Node)) {
@@ -151,8 +310,9 @@ const Navbar: FC = () => {
 
   return (
     <>
-      <nav className="bg-white shadow-md px-5 md:px-16 py-3 flex justify-between items-center font-inter"> {/* The shadow-md to see the limits of navbar */}
-        <div className="flex space-x-20">
+      <nav className="bg-white/80 backdrop-blur-md border-b px-5 md:px-8 xl:px-16 py-2 flex justify-between items-center font-inter fixed top-0 left-0 z-50 w-full"> {/* The shadow-md to see the limits of navbar */}
+        {/* Logo */}
+        <div className="flex space-x-10 xl:space-x-20">
             <Link href="/" passHref>
               <div className='flex items-center space-x-4 cursor-pointer'>
                 <Image 
@@ -167,9 +327,15 @@ const Navbar: FC = () => {
                 </span>
               </div>
             </Link>
+
+
+          {/* Desktop */}
+          <div className="hidden lg:flex items-center space-x-10">
           {isLoggedIn && (
-            <div className='flex items-center space-x-10 mt-1.5'>
-              <div className='relative' ref={myBalanceDropdownRef}>
+            <div className='flex items-center space-x-5 xl:space-x-10 mt-1.5'>
+              <div className='relative' ref={myBalanceDropdownRef}
+
+              >
               <DropdownButton
                 label="My Balance"
                 onClick={() => setIsMyBalanceDropdownOpen((prev) => !prev)}
@@ -185,7 +351,8 @@ const Navbar: FC = () => {
               />
               )}
               </div>
-              <div className="relative" ref={familyDropdownRef}>
+              <div className="relative" ref={familyDropdownRef}
+              >
               <DropdownButton
                 label="My Family"
                 onClick={() => setIsFamilyDropdownOpen((prev) => !prev)}
@@ -206,7 +373,8 @@ const Navbar: FC = () => {
                 />
                 )}
               </div>
-              <div className="relative" ref={learningDropdownRef}>
+              <div className="relative" ref={learningDropdownRef}
+              >
               <DropdownButton
                 label="Learning Center"
                 onClick={() => setIsLearningDropdownOpen((prev) => !prev)}
@@ -239,8 +407,32 @@ const Navbar: FC = () => {
             </div>
           )}
         </div>
+        </div>
+        {/* Hamburger Menu */}
+        <div className="flex lg:hidden z-50">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="bg-brightRed/10 rounded-md p-2 focus:outline-none hover:brightness-110 transition-transform duration-300 ease-in-out shadow-sm"
+          >
+            <svg
+              className="w-7 h-7 text-brightRed drop-shadow"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
+            />
+            </svg>
+          </button>
+        </div>
 
-        <div className="flex items-center space-x-7 mt-1.5">
+        {/* Desktop Not loggedIn */}
+        <div className="lg:flex hidden items-center space-x-7 mt-1.5">
           {!isLoggedIn ? (
             <>
               <Link href="/login" passHref>
@@ -254,10 +446,10 @@ const Navbar: FC = () => {
             </>
           ) : (
             <>
-              <span className="text-gray-800 font-semibold">Welcome, User!</span>
+              <span className="text-gray-800 font-semibold">Welcome, <span className='text-brightGreen'>User!</span></span>
               <button
                 onClick={handleLogout}
-                className="flex items-center font-semibold space-x-2 text-gray-800 hover:text-brightRed transition duration-300 ease-in-out"
+                className="flex items-center font-semibold space-x-2 text-gray-800 hover:text-brightGreen transition duration-300 ease-in-out"
               >
                 <span>Logout</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
@@ -266,8 +458,70 @@ const Navbar: FC = () => {
               </button>
             </>
           )}
+          
         </div>
       </nav>
+
+      {/* Mobile Variant */}
+{isOpen && (
+  <div className="fixed inset-0 z-40 flex">
+    {/* Overlay */}
+    <div
+      className="absolute inset-0 bg-black/70"
+      onClick={() => setIsOpen(false)}
+    ></div>
+
+    {/* Mobile/Tablet Menu */}
+    <div className="absolute top-0 right-0 w-full md:w-1/2 h-full bg-white p-6 shadow-lg transition-transform duration-300">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-lg font-bold">Menu</h2>
+        <button
+          onClick={() => setIsOpen(false)}
+          className="p-2 text-gray-600 hover:text-brightRed transition"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+      <nav className="flex flex-col space-y-4">
+        {!isLoggedIn ? (
+          <>
+            <Link href="/login" passHref>
+              <span className="text-gray-800 hover:text-brightRed transition duration-300 ease-in-out cursor-pointer">Log in</span>
+            </Link>
+            <Link href="/signup" passHref>
+              <span className="bg-brightRed text-white px-4 py-2 rounded-md hover:brightness-110 cursor-pointer transition ease-in-out duration-300">
+                Sign up
+              </span>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link href="/profile" passHref>
+              <span className="text-gray-800 hover:text-brightGreen transition duration-300 ease-in-out cursor-pointer">
+                Profile
+              </span>
+            </Link>
+            <button
+              onClick={handleLogout}
+              className="flex items-center font-semibold space-x-2 text-gray-800 hover:text-brightGreen transition duration-300 ease-in-out"
+            >
+              <span>Logout</span>
+            </button>
+          </>
+        )}
+      </nav>
+    </div>
+  </div>
+)}
     </>
   );
 };
