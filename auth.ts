346ee74da486f853976e2452a679c8d31252e207
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-12-06 19:44:18 -0600

    forgot to uncomment the authentication

--- auth.ts
+++ auth.ts
@@ -1,50 +1,50 @@
 {/* Commented out to be able to do the frontend work */}
 
-// import { NextAuthOptions } from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import Credentials from "next-auth/providers/credentials";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// import { prisma } from "@/prisma";
-// import bcrypt from "bcryptjs";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/prisma";
+import bcrypt from "bcryptjs";
 
-// export const authOptions: NextAuthOptions = {
-//   adapter: PrismaAdapter(prisma),
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-//     }),
-//     Credentials({
-//       name: "Credentials",
-//       credentials: {
-//         email: { label: "Email", type: "email" },
-//         password: { label: "Password", type: "password"},
-//       },
-//       async authorize(credentials) {
-//         const user = await prisma.parent_user.findUnique({
-//           where: { email: credentials?.email },
-//         });
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password"},
+      },
+      async authorize(credentials) {
+        const user = await prisma.parent_user.findUnique({
+          where: { email: credentials?.email },
+        });
 
-//         if (user && user.password) {
-//           const isPasswordValid = await bcrypt.compare(credentials?.password!, user.password);
+        if (user && user.password) {
+          const isPasswordValid = await bcrypt.compare(credentials?.password!, user.password);
 
-//           if (isPasswordValid) {
-//             return user;
-//           }
-//         }
+          if (isPasswordValid) {
+            return user;
+          }
+        }
 
-//         return null;
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     session: async ({ session, token, user }) => {
-//       if (session.user) {
-//         session.user.id = user.id;
-//         session.user.email = user.email;
-//         session.user.name = user.name;
-//       }
-//       return session;
-//     },
-//   },
-// };
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    session: async ({ session, token, user }) => {
+      if (session.user) {
+        session.user.id = user.id;
+        session.user.email = user.email;
+        session.user.name = user.name;
+      }
+      return session;
+    },
+  },
+};
---
+++ middleware.ts
@@ -0,0 +1,13 @@
+{/* Commenting out to do the backend work */}
+
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login"
+  }
+})
+
+export const config = {
+  matcher: ["/home/:path*", "/signup/google"],
+};
