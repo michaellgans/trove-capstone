ada22d1d3ad2271340abe70b0422d1a89dd26581
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-12-07 17:07:15 -0600

    some additional styling on cards, responsive text sizes etc

--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -85,10 +85,10 @@ const Settings: React.FC = () => {
     return (
         <div
         ref={cardRef}
-        className="flex flex-col bg-white p-10 rounded-lg border border-gray-100 shadow-lg max-w-md lg:max-w-2xl mx-auto mt-6">
+        className="flex flex-col bg-white p-5 md:p-10 rounded-lg border border-gray-100 shadow-lg max-w-md md:max-w-xl lg:max-w-2xl mx-auto mt-6">
             <header className="items-start mb-6">
-                <h1 className="text-4xl font-inter text-text text-left">Settings</h1>
-                <div className="w-48 h-1 mt-2 rounded-full overflow-hidden flex">
+                <h1 className="text-2xl md:text-3xl lg:text-4xl font-inter text-text text-left">Settings</h1>
+                <div className="w-28 sm:w-32 md:w-36 lg:w-48 h-1 mt-2 rounded-full overflow-hidden flex">
                     <div className="flex-1 bg-brightRed"></div>
                     <div className="flex-1 bg-brightYellow"></div>
                     <div className="flex-1 bg-brightBlue"></div>
