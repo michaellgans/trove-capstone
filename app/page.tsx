bc812d996ca71118f28983a73e176160b7317c13
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-12-10 17:20:13 -0600

    landing page marketing section added with some blur on top for smoother transition of sections

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,9 @@ export default function LandingPage() {
     
     <Carousel />
     {/* Banner Image */}
-    <div className="relative w-full">
-    
+    <div className="relative w-full mt-5">
+      {/* Blurry Overlay */}
+      <div className="absolute inset-x-0 top-0 h-[100px] md:h-[150px] lg:h-[200px] bg-gradient-to-b from-white via-white/70 to-transparent z-10 pointer-events-none"></div>
         <Image
           src="/images/advertisement_web.png"
           alt="Advertisement Banner"
